25b485712bb6fcb21390edfa57a4cb0b2252d655
ccoenraets <ccoenraets@gmail.com>
2016-09-26 08:59:11 -0400

    vision service

--- modules/uploads.js
+++ modules/uploads.js
@@ -5,11 +5,15 @@ let formatter = require('./formatter');
 let visionService = require('./vision-service');
 
 exports.processUpload = (sender, attachments) => {
-    if (attachments.length > 0 && attachment.type === "image") {
+    if (attachments.length > 0) {
         let attachment = attachments[0];
-        messenger.send({text: `OK, looking for houses like that...`}, sender);
-        visionService.classify(attachment.url)
-            .then(houseType => salesforce.findPropertiesByCategory(houseType))
-            .then(properties => messenger.send(formatter.formatProperties(properties), sender))
+        if (attachment.type === "image") {
+            messenger.send({text: `OK, looking for houses like that...`}, sender);
+            visionService.classify(attachment.url)
+                .then(houseType => salesforce.findPropertiesByCategory(houseType))
+                .then(properties => messenger.send(formatter.formatProperties(properties), sender))
+        } else {
+            messenger.send({text: `This type of attachment is not supported`}, sender);
+        }
     }
 };
