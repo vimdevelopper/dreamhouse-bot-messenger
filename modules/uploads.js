f1545ecdad284a3b85332a474151889e6a237f6f
Christophe Coenraets <ccoenraets@ccoenraets-ltm1.internal.salesforce.com>
2016-10-04 22:20:44 -0700

    formatting messages

--- modules/uploads.js
+++ modules/uploads.js
@@ -9,9 +9,12 @@ exports.processUpload = (sender, attachments) => {
     if (attachments.length > 0) {
         let attachment = attachments[0];
         if (attachment.type === "image") {
-            messenger.send({text: 'OK, looking for houses like that...'}, sender);
+            messenger.send({text: 'OK, let me look at that picture...'}, sender);
             visionService.classify(attachment.url)
-                .then(houseType => salesforce.findPropertiesByCategory(houseType))
+                .then(houseType => {
+                    messenger.send({text: `Looking for houses matching "${houseType}"`}, sender);
+                    salesforce.findPropertiesByCategory(houseType)
+                })
                 .then(properties => messenger.send(formatter.formatProperties(properties), sender))
         } else {
             messenger.send({text: 'This type of attachment is not supported'}, sender);
