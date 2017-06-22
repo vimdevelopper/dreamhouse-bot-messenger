759ff9c7b583d3c4c60e31e2e27a4c7c1575b358
vimdevelopper <vimdevelopper@gmail.com>
2017-06-22 16:31:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
 app.get('/webhook', (req, res) => {
     if (req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
         res.send(req.query['hub.challenge']);
